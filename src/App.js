d08ffedbfb38e6c4a7e51b29ffe67da34c4e6b62
Akbarss <kbrgulyamov@gmail.com>
2024-04-02 10:16:48 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import TodoApp from "./Todo";
 
 function App() {
+  const [name, setName] = useState("Shahzod");
+  //Product State
+  const [product, setProduct] = useState([
+    { id: 1, name: "Americano", price: 400 },
+    { id: 2, name: "Latte", price: 300 },
+    { id: 3, name: "Cappuccino", price: 500 },
+    { id: 4, name: "Espresso", price: 100 },
+  ]);
+
+  const handleChangeName = () => {
+    setName("Komil");
+  };
+
+  const handleBuy = (productId) => {
+    const p = product.find((i) => i.id === productId);
+    if (p) {
+      alert(`Siz ${p.name} sotib oldingiz. Narxi: ${p.price} dollar`);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name}</h1>
+      <button onClick={handleChangeName}>click</button>
+
+      <h1>Menu Qahvaxona</h1>
+      <ul>
+        {product.map((i) => (
+          <>
+            <li>{i.name}</li>
+            <li>{i.price}</li>
+            <button onClick={() => handleBuy(i.id)}>Sotib olish</button>
+          </>
+        ))}
+      </ul>
+      <TodoApp />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoApp from "./Todo";
+
+const Home = () => {
+  return (
+    <>
+      <TodoApp />
+    </>
+  );
+};
+
+export default Home;
