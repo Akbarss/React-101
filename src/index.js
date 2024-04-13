54325d94435475ad1b65185d4692195e855b2322
Akbarss <kbrgulyamov@gmail.com>
2024-04-13 11:43:53 +0500

    add btn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BitcoinPriceApp from "./components/Bit";
+import BitcoinPrice from "./components/BitcoinPrice";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BitcoinPriceApp />
+    <BitcoinPrice />
   </React.StrictMode>
 );
